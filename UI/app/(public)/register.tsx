023a2b413f5469afd360535c48ed2b0c8394fbfc
DoanManhDuong <21020295@vnu.edu.vn>
2024-04-27 09:01:50 +0700

    update UI login register

--- UI/app/(public)/register.tsx
+++ UI/app/(public)/register.tsx
@@ -72,18 +72,20 @@ const register = () => {
             style={styles.inputField}
           />
           <TextInput
-            placeholder="Password"
+            placeholder="Mật khẩu"
             value={password}
             onChangeText={setPassword}
             secureTextEntry
             style={styles.inputField}
           />
 
-          <Button
-            onPress={onSignUpPress}
-            title="Sign up"
-            color={"#2E82FF"}
-          ></Button>
+          <View style={styles.button}>
+            <Button
+              onPress={onSignUpPress}
+              title="Đăng ký"
+              color={"#2E82FF"}
+            ></Button>
+          </View>
         </>
       )}
 
@@ -92,16 +94,18 @@ const register = () => {
           <View>
             <TextInput
               value={code}
-              placeholder="Code..."
+              placeholder="Mã xác thực..."
               style={styles.inputField}
               onChangeText={setCode}
             />
           </View>
-          <Button
-            onPress={onPressVerify}
-            title="Verify Email"
-            color={"#2E82FF"}
-          ></Button>
+          <View style={styles.button}>
+            <Button
+              onPress={onPressVerify}
+              title="Xác thực Email"
+              color={"#2E82FF"}
+            ></Button>
+          </View>
         </>
       )}
     </View>
@@ -117,16 +121,16 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   inputField: {
-    marginVertical: 4,
+    marginVertical: 8,
     height: 50,
     borderWidth: 1,
     borderColor: "#2E82FF",
-    borderRadius: 4,
+    borderRadius: 8,
     padding: 10,
     backgroundColor: "#fff",
   },
   button: {
-    margin: 8,
-    alignItems: "center",
+    marginVertical: 8,
+    borderRadius: 8,
   },
 });
