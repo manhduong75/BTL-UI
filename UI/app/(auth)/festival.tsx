9119eb36e469c4750025e25ba13a0888dc5e0bb5
DoanManhDuong <21020295@vnu.edu.vn>
2024-04-28 16:17:52 +0700

    fix scrollview error

--- UI/app/(auth)/festival.tsx
+++ UI/app/(auth)/festival.tsx
@@ -1,12 +1,21 @@
-import React, {useState} from "react";
-import { StyleSheet, View, TextInput, TouchableOpacity, Text, Image, FlatList, Button, ScrollView } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Image,
+  FlatList,
+  Button,
+  ScrollView,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const Festival = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchText, setSearchText] = useState("");
-  
 
   const handleSearch = () => {
     console.log("Giá trị searchText:", searchText);
@@ -14,39 +23,39 @@ const Festival = () => {
 
   const [data] = useState([
     {
-      "id": 1,
-      "text": "Lễ hội Hoa Ban",
-      "uri": "https://vnmedia.vn/file/8a10a0d36ccebc89016ce0c6fa3e1b83/8a10a0d36d961641016db9c9fa273c32/022022/1_20220214152059.jpg"
+      id: 1,
+      text: "Lễ hội Hoa Ban",
+      uri: "https://vnmedia.vn/file/8a10a0d36ccebc89016ce0c6fa3e1b83/8a10a0d36d961641016db9c9fa273c32/022022/1_20220214152059.jpg",
     },
     {
-      "id": 2,
-      "text": "Lễ hội Hoa Anh Đào",
-      "uri": "https://afamilycdn.com/150157425591193600/2023/12/10/3281055004221258734345941997230514713636857n-1-1702166075638480154269-1702178556587-17021785579251079420961.jpg"
+      id: 2,
+      text: "Lễ hội Hoa Anh Đào",
+      uri: "https://afamilycdn.com/150157425591193600/2023/12/10/3281055004221258734345941997230514713636857n-1-1702166075638480154269-1702178556587-17021785579251079420961.jpg",
     },
     {
-      "id": 3,
-      "text": "Chợ phiên Tủa Chùa",
-      "uri": "https://i.ytimg.com/vi/DDvNhP-R0eY/sddefault.jpg"
+      id: 3,
+      text: "Chợ phiên Tủa Chùa",
+      uri: "https://i.ytimg.com/vi/DDvNhP-R0eY/sddefault.jpg",
     },
     {
-      "id": 4,
-      "text": "Lễ hội Thành Bản Phù",
-      "uri": "https://baovanhoa.vn/Portals/0/EasyDNNnews/51472/A1.jpg"
+      id: 4,
+      text: "Lễ hội Thành Bản Phù",
+      uri: "https://baovanhoa.vn/Portals/0/EasyDNNnews/51472/A1.jpg",
     },
     {
-      "id": 5,
-      "text": "Kỷ niệm chiến thắng Điện Biên Phủ",
-      "uri": "https://mediafile.qdnd.vn//images/2024/3/12/cover-3.jpg"
+      id: 5,
+      text: "Kỷ niệm chiến thắng Điện Biên Phủ",
+      uri: "https://mediafile.qdnd.vn//images/2024/3/12/cover-3.jpg",
     },
     {
-      "id": 6,
-      "text": "Tết cổ truyền",
-      "uri": "https://gdtd.1cdn.vn/2023/11/28/khong-khi-tet.jpeg"
+      id: 6,
+      text: "Tết cổ truyền",
+      uri: "https://gdtd.1cdn.vn/2023/11/28/khong-khi-tet.jpeg",
     },
     {
-      "id": 7,
-      "text": "Hương sắc Điện Biên",
-      "uri": "https://imagev3.dantocmiennui.vn/w1000/Uploaddtmn/2017/3/17/85-1.JPG"
+      id: 7,
+      text: "Hương sắc Điện Biên",
+      uri: "https://imagev3.dantocmiennui.vn/w1000/Uploaddtmn/2017/3/17/85-1.JPG",
     },
   ]);
 
@@ -62,8 +71,12 @@ const Festival = () => {
   //   setCurrentPage(page);
   // };
 
-  const renderImageItem = ({ item }: { item: { id: number, uri: string, text: string } }) => (
-    <View style={styles.imageContainer} >
+  const renderImageItem = ({
+    item,
+  }: {
+    item: { id: number; uri: string; text: string };
+  }) => (
+    <View style={styles.imageContainer}>
       <TouchableOpacity>
         <Image source={{ uri: item.uri }} style={styles.image} />
         <Text style={styles.text}> {item.text}</Text>
@@ -141,48 +154,52 @@ const Festival = () => {
   // };
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.searchContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Nhập từ khóa..."
-            value={searchText}
-            onChangeText={setSearchText}
-          />
-          <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
-            <Ionicons name="search" size={24} color="black" style={styles.searchIcon} />
-          </TouchableOpacity>
+    <FlatList
+      data={data}
+      renderItem={renderImageItem}
+      keyExtractor={(item) => item.id.toString()}
+      numColumns={itemsPerRow}
+      contentContainerStyle={styles.list}
+      ListHeaderComponent={
+        <View style={styles.container}>
+          <View style={styles.searchContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Nhập từ khóa..."
+              value={searchText}
+              onChangeText={setSearchText}
+            />
+            <TouchableOpacity
+              style={styles.searchButton}
+              onPress={handleSearch}
+            >
+              <Ionicons
+                name="search"
+                size={24}
+                color="black"
+                style={styles.searchIcon}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      <FlatList
-        data={data}
-        renderItem={renderImageItem}
-        keyExtractor={item => item.id.toString()}
-        numColumns={itemsPerRow}
-        contentContainerStyle={styles.list}
-      />  
-      {/* <View style={styles.paginationContainer}>
-        {renderPaginationButtons()}
-      </View> */}
-    </ScrollView>
+      }
+    />
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 20,
   },
   searchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     borderWidth: 1,
     borderRadius: 20,
-    borderColor: 'gray',
+    borderColor: "gray",
     paddingHorizontal: 10,
     flex: 1,
     marginHorizontal: 30,
@@ -202,8 +219,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
   },
   list: {
     padding: 10,
@@ -211,24 +228,24 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
-    margin: 10, 
-    alignItems: 'center',
+    margin: 10,
+    alignItems: "center",
   },
   image: {
     width: 150,
-    height: 150, 
-    borderRadius: 10, 
-    resizeMode: 'cover',
+    height: 150,
+    borderRadius: 10,
+    resizeMode: "cover",
   },
   text: {
     marginTop: 5,
-    textAlign: 'center',
+    textAlign: "center",
     width: 150,
   },
   paginationContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 10,
   },
 });
